6e3e32b91ef2eb670c88dba3f17401e8f0c58cc7
Rye-Ton <tcriddle@smu.edu>
2019-03-28 09:58:58 -0500

    Creation of AccountView docs

--- jumpercables/src/App.js
+++ jumpercables/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import './account/AccountView'
+import AccountView from './account/AccountView';
 
 class App extends Component {
   render() {
     return (
         <>
+          <AccountView></AccountView>
         </>
     );
   }
---
+++ jumpercables/src/account/AccountView.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './styles/AccountView.css';
+
+const AccountView = (props) => {
+    return (
+        <>
+        <div>
+        </div>
+        </>
+    );
+}
+
+function userInfo(props) {
+    return (
+        <div className="User">
+        Ayo
+        </div>
+    )
+}
+
+export default AccountView;
