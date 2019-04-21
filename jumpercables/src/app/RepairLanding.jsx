9d16db2da97e23c23aea64d3e98ade34cd7f9368
Rye-Ton <tcriddle@smu.edu>
2019-04-20 19:36:10 -0500

    File renaming

--- jumpercables/src/app/RepairLanding.jsx
+++ jumpercables/src/app/RepairLanding.jsx
@@ -1,9 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import RepairList from "./RepairList";
+import RepairForm from "./RepairForm";
 
 export class RepairLanding extends Component {
-    render () {
+    state = {
+        repairs: []
+    };
+
+    onNew(repair) {
+        this.setState(prevState => {
+            prevState.repairs.push(repair);
+            return prevState;
+        });
+    }
+
+    render() {
         return (
             <>
+                <div className="container d-flex flex-column align-items-center">
+                    <h1> Repairs </h1>
+                    <RepairList repairs={this.state.repairs}/>
+                    <RepairForm className="" onNew={a => this.onNew(a)}/>
+                </div>
             </>
         );
     }
