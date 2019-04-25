d60d66fb1b76d3d48ac1d6bd572e3aa5875a4363
bsaunderss <35714573+bsaunderss@users.noreply.github.com>
2019-04-24 19:50:58 -0500

    Add files via upload

--- jumpercables/src/models/Repair.js
+++ jumpercables/src/models/Repair.js
@@ -1,7 +1,8 @@
 export class Repair {
-    constructor(car, repairType, repairCost) {
-        this.car = car;
-        this.repairType = repairType;
-        this.repairCost = repairCost;
-    }
+  constructor(car, repairType, repairCost, repairDesc) {
+    this.car = car;
+    this.repairType = repairType;
+    this.repairCost = repairCost;
+    this.repairDesc = repairDesc;
+  }
 }
