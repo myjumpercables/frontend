dc554c738912dde403e8a845d4cb0d68aff9560c
Ryan-Kennedy <kennedy@smu.edu>
2019-04-30 21:50:30 -0500

    fixed repair form

--- jumpercables/src/app/ServiceRepairList.jsx
+++ jumpercables/src/app/ServiceRepairList.jsx
@@ -1,63 +1,70 @@
 import React from "react";
 
 export const ServiceRepairList = (props) => {
-    if (!props.repairs)
+    props.cars.map((car, i) =>
+        console.log(car.make)
+    )
+    if (!props.cars)
         return (
             <div className="container rounded mt-1 mb-1">
                 <div
                     className="navbar navbar-light"
-                    style={{ backgroundColor: " #f2f2f2" }}>
+                    style={{backgroundColor: " #f2f2f2"}}>
                     Add a repair!
                 </div>
             </div>
         );
-    return(
-        <div className='container'>
-            <div className='row'>
+    return (
+        <div className='container '>
+            <div className='row justify-content-center'>
                 {
-                props.car.services.map((service, i) =>
-                    <div className='col-4'>
-                        <div className='card bg-light'>
-                        <div className='card-header border-bottom' key={i}>
-                            <h4>{props.car.year} {props.car.make} {props.car.model}</h4>
-                        </div>
-                            <div id='accordion'>
-                                {!!props.   car.services.length && props.cars.services.map((service, j) =>
-                                    <div id="accordion">
-                                        <div className="card">
-                                            <div className="card-header" id={`heading${j}`} key={j}>
-                                                <h5 className="mb-0">
-                                                    <button className="btn btn-info w-100" data-toggle="collapse"
-                                                            data-target={`#service${i}${j}`}
-                                                            aria-controls="collapseOne">
-                                                        <strong> Service: </strong> {service.subject}
-                                                    </button>
-                                                </h5>
-                                            </div>
-
-                                            <div id={`service${i}${j}`} className="collapse"
-                                                 aria-labelledby={`heading${j}`} data-parent="#accordion">
-                                                <div className="card-body" id={`accordion${i}${j}`}>
-                                                    <div className="card-body">
-                                                        {`The repair is: {repair.repairType}`}
-                                                    </div>
-                                                    <div className='card-footer'>
-                                                        {`Total Cost: {repair.repairCost}`}
-                                                    </div>
-                                                    <div>
-                                                        <i className="fas fa-check"></i>
-                                                        <i className="fas fa-times-circle"></i>
-                                                    </div>
+                    props.cars.map((car, i) =>
+                        <div className='col-6'>
+                            <div className='card bg-light'>
+                                <div className='card-header border-bottom' key={i}>
+                                    <h4>{car.year} {car.make} {car.model}</h4>
+                                </div>
+                                <div id='accordion'>
+                                    {car.services.map((service, j) =>
+                                        <div id="accordion" key={j}>
+                                            <div className="card">
+                                                <div className="card-header" id={`heading${j}`}>
+                                                    <h5 className="mb-0">
+                                                        <button className="btn btn-info w-100" data-toggle="collapse"
+                                                                data-target={`#service${i}${j}`}
+                                                                aria-controls="collapseOne">
+                                                            <strong> Service: </strong> {service.service_type}
+                                                        </button>
+                                                    </h5>
                                                 </div>
+                                                {!!service.repairs.length && service.repairs.map((repair, k) =>
+
+                                                    <div id={`service${j}${k}`} className="collapse"
+                                                         aria-labelledby={`heading${j}`} data-parent="#accordion" key={k}>
+                                                        <div className="card-body" id={`accordion${i}${j}`}>
+                                                            <div className="card-header">
+                                                                {`The repair costs: ${repair.cost}`}
+                                                            </div>
+                                                            <div className='card-body'>
+                                                                {repair.repair_desc}
+                                                            </div>
+                                                            <div className='row justify-content-center'>
+                                                                <button>
+                                                                    <i className="fas fa-check bg-success"></i>
+                                                                </button>
+                                                                <button>
+                                                                    <i className="fas fa-times-circle bg-danger"></i>
+                                                                </button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                )}
                                             </div>
                                         </div>
-                                    </div>
-                                )}
+                                    )}</div>
                             </div>
-                        </div>
-                    </div>
-                )
-            }
+                        </div>)
+                }
             </div>
         </div>
     )
